5dfa96d947a2a0f32444cf7677c164260ce7e5d6
Jeongmin Cho <jeongcho96@gmail.com>
2019-07-02 06:47:08 -0400

    added timer, progressbar, styles

--- src/components/TimerClock.js
+++ src/components/TimerClock.js
@@ -1,15 +1,62 @@
 import React from "react";
-import ReactCountdownClock from "react-countdown-clock";
+import TimerProgressBar from "./TimerProgressBar.js";
+// import ReactCountdownClock from "react-countdown-clock";
 
 class TimerClock extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      time: 1000
+      min: 25,
+      sec: 0,
+      timerOn: false
     };
   }
+
+  stopInterval() {
+    clearInterval(this.timerID);
+    this.setState({
+      timerOn: false
+    });
+  }
+
+  startInterval() {
+    this.timerID = setInterval(() => {
+      if (this.state.sec === 0) {
+        this.setState({ min: this.state.min - 1, sec: 59 });
+      } else {
+        this.setState({ sec: this.state.sec - 1 });
+      }
+    }, 250);
+    this.setState({
+      timerOn: true
+    });
+  }
+
   render() {
-    return <div id="timerClockContainer" />;
+    return (
+      <div id="timerClock">
+        <TimerProgressBar min={this.state.min} sec={this.state.sec} />
+        {this.state.timerOn ? (
+          <button
+            id="timerButton"
+            onClick={() => {
+              this.stopInterval();
+            }}
+          >
+            PAUSE
+          </button>
+        ) : (
+          <button
+            id="timerButton"
+            onClick={() => {
+              this.startInterval();
+            }}
+          >
+            START
+          </button>
+        )}
+      </div>
+    );
   }
 }
 
---
+++ src/components/TimerProgressBar.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+// Import module and default styles
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+const needDominantBaselineFix = true;
+
+const TimerProgressBar = props => {
+  const min = props.min;
+  const sec = props.sec;
+  const total = 25 * 60;
+  const current = min * 60 + sec;
+  const percentage = (current / total) * 100;
+  return (
+    <div style={{ width: "430px" }}>
+      <CircularProgressbar
+        value={percentage}
+        circleRatio={0.85}
+        strokeWidth={1.5}
+        styles={buildStyles({
+          rotation: 0.575,
+          strokeLinecap: "round",
+          textSize: "25px",
+          pathTransitionDuration: 0.5,
+          pathColor: "#FFFFFF",
+          textColor: "#FFFFFF",
+          trailColor: "#FF8389",
+          text: {
+            fill: "#FFFFFF",
+            fontSize: "16px"
+          }
+        })}
+        text={
+          sec < 10 ? (
+            <tspan dy={needDominantBaselineFix ? -15 : 0} id="timerText">
+              {min}:0{sec}
+            </tspan>
+          ) : (
+            <tspan dy={needDominantBaselineFix ? -15 : 0} id="timerText">
+              {min}:{sec}
+            </tspan>
+          )
+        }
+      />
+    </div>
+  );
+};
+
+// const TimerProgressBar = props => {
+//   const min = props.min;
+//   const sec = props.sec;
+// const total = 25 * 60;
+// const current = min * 60 + sec;
+//   const rate = (current / total) * 100;
+//   return (
+//     <div id="timerProgressContainer">
+//       <div id="timerProgressBar" style={{ width: `${rate}%` }} />
+//       <canvas id="myCanvas" width="500" height="500" />
+//     </div>
+//   );
+// };
+
+export default TimerProgressBar;
